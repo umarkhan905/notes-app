258432470a8ddd0aa39c51c2a35008b898abd2b2
umarkhan905 <itxumarkhan905@gmail.com>
2025-04-18 12:41:44 +0500

    Adding timestamp in backend model.

--- backend/src/models/note.model.js
+++ backend/src/models/note.model.js
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
 
-const noteSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
   },
-  content: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Note = mongoose.model("Note", noteSchema);
 
